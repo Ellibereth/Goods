7c380a1e5b32e678ab39c8846c3836d976d1b612
Darek <darek@manaweb.com>
2017-03-15 17:37:38 -0400

    just click on table elements to show more or hide for large entries

--- web/static/components/Admin/AdminTools.js
+++ web/static/components/Admin/AdminTools.js
@@ -18,7 +18,6 @@ export default class AdminTools extends React.Component {
 			  type: "POST",
 			  url: real_url + "/getProductRequests",
 			  success: function(data) {
-				console.log(data)
 				that.setState({product_requests: data})
 			  },
 			  error : function(){
@@ -30,19 +29,51 @@ export default class AdminTools extends React.Component {
 			return product_requests
 	}
 
+	shortenText(text){
+		var maxLength = 40
+		if (text.length < maxLength) {
+			return text
+		}
+		else {
+			return text.substring(0,maxLength - 4) + "..."
+		}
+	}
+
+	// hides if the text is less than 40
+	// otherwise shows all
+	toggleText(s_id, index, attr){
+		var id = s_id + "_" + attr
+		var product_requests = this.state.product_requests
+		if ($("#" + id).text().length > 40) {
+			$("#" + id).text(this.shortenText($("#" + id).text()))
+		}
+		else {
+			$("#" + id).text(product_requests[index][attr])
+		}
+	}
+
 
 	render() {
 		var product_requests = this.state.product_requests
-		console.log(product_requests[0])
 		var request_variables = ['submission_id', 'name', 'email', 'product_description', 'price_range', 'confirmed', 'completed']
 		var headers = ['Submission Id', 'Name', 'Email', 'Product Description', 'Price Range', 'Confirmed', 'Completed']
 		var table_headers = headers.map((header) => 
 				<th> {header} </th>
 			)
+		
+		const td_styles = {
+			"word-wrap" : "break-word",
+			"min-width" : "160px",
+			"max-width" : "160px"
+		}
+
 		var table_entries = product_requests.map((request) => 
 				{	
-					var rows = request_variables.map((attr) =>
-							<td> {request[attr].toString()} </td> 
+					var rows = request_variables.map((attr, index) =>
+							<td id = {request['submission_id'] + "_" + attr} s_id = {request['submission_id']}
+							attr = {attr} style = {td_styles} index = {index}
+							onClick = {() => this.toggleText.bind(this)(request['submission_id'], index, attr)}> 
+							{this.shortenText(request[attr].toString())} </td> 
 						)
 					return (
 						<tr>
@@ -52,16 +83,18 @@ export default class AdminTools extends React.Component {
 			})
 		return (
 			<div className = "container">
-				<table className ="table table-bordered">
-					<thead>
-						<tr>
-							{table_headers}
-						</tr>
-					</thead>
-					<tbody>
-						{table_entries}
-					</tbody>
-				</table>
+				<div className="col-md-12">
+					<table className ="table table-bordered">
+						<thead>
+							<tr>
+								{table_headers}
+							</tr>
+						</thead>
+						<tbody>
+							{table_entries}
+						</tbody>
+					</table>
+				</div>
 			</div>
 		);
 	}
