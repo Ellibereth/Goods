79bd6d96794bd6d8317ed307914d6cbe97fdbd02
Darek <darek@manaweb.com>
2017-06-22 19:57:38 -0400

    more customer service pages

---
+++ web/static/components/CustomerService/FaqPage.js
@@ -0,0 +1,29 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var browserHistory = require('react-router').browserHistory
+import PageContainer from '../Misc/PageContainer'
+import {AlertMessages} from '../Misc/AlertMessages'
+
+export default class FaqPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+
+	render() {
+		var component = (
+			<div className = "container">
+				<h1> FAQ </h1>
+			</div>
+
+		)
+
+		return (
+				<PageContainer component = {component}/>
+		);
+	}
+}
