447f038d714fcc0795b17a401e17c558cfae349b
Darek <darek@manaweb.com>
2017-07-18 20:24:40 -0400

    reformatting home image

--- web/static/components/Home/HomePageSingleImage.js
+++ web/static/components/Home/HomePageSingleImage.js
@@ -4,7 +4,7 @@ var base_url = "https://s3-us-west-2.amazonaws.com/edgarusahomepage/"
 
 
 // temporary hard coded image
-const img_src = "https://s3-us-west-2.amazonaws.com/edgarusahomepage/DEVELOPMENT/HUKM8ET2VY6EIOSX6LSZ"
+const img_src = "https://s3-us-west-2.amazonaws.com/edgarusahomepage/4.jpg"
 
 export default class HomePageImageCarousel extends React.Component {
 	constructor(props) {
