42ddd9d4f99db81d8e16faf5f4b71aed685db695
Darek <darek@manaweb.com>
2017-08-21 15:17:04 -0400

    changing the month since we're behind schedule :(

--- web/static/components/Landing/LandingPage.js
+++ web/static/components/Landing/LandingPage.js
@@ -172,7 +172,7 @@ export default class LandingPage extends React.Component {
 
 						<div className = "row">
 							<div className = "landing-after-input-text">
-								Coming soon, August 2017
+								Coming soon, September 2017
 							</div>
 						</div>
 
